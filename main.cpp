51024dca487a0c2c78a1e6eac1ed17496a959815
yazeedshahin-27 <yazeed.farouk@gmail.com>
2026-07-06 18:26:59 +0300

    Add menu-based student management functionality

--- main.cpp
+++ main.cpp
@@ -6,16 +6,44 @@ public:
     int id;
     string name;
 
+    void input() {
+        cout << "Enter ID: ";
+        cin >> id;
+        cin.ignore();
+        cout << "Enter name: ";
+        getline(cin, name);
+    }
+
     void display() {
-        cout << "ID: " << id << endl;
-        cout << "Name: " << name << endl;
+        cout << "ID: " << id << ", Name: " << name << endl;
     }
 };
 
 int main() {
-    Student s;
-    s.id = 1;
-    s.name = "Test Student";
-    s.display();
+    Student students[50];
+    int count = 0;
+    int choice;
+
+    do {
+        cout << "\n--- Student Management System ---\n";
+        cout << "1. Add Student\n";
+        cout << "2. View Students\n";
+        cout << "3. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            students[count].input();
+            count++;
+            cout << "Student added successfully.\n";
+        } 
+        else if (choice == 2) {
+            for (int i = 0; i < count; i++) {
+                students[i].display();
+            }
+        }
+
+    } while (choice != 3);
+
     return 0;
 }
